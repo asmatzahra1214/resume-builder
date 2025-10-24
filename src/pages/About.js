84fbfd6ffd7e572c0e5c0bce34cabcd0939807bf
asmatzahra1214 <asmatzahranaqvi1214@gmail.com>
2025-10-24 10:34:13 -0700

    pages linked and navbar created successfully by asmatzahranaqvi

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className=' flex justify-center items-center min-h-screen '>
+      Hy this is about page....
+    </div>
+  )
+}
+
+export default About
